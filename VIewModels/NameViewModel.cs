b1b8163edef18f7390ffe79a9486f458e3bf14bf
Ana Antic <aantic@schueler.hakzell.at>
2024-04-19 13:05:15 +0200

    create class

--- VIewModels/NameViewModel.cs
+++ VIewModels/NameViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,10 @@ using System.Threading.Tasks;
 
 namespace MauiAppName.VIewModels;
 
-public class NameViewModel
+public partial class NameViewModel : ObservableObject
 {
+    [ObservableProperty]
+    string _name = string.Empty;
+
 
 }
